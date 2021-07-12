0c2490b312190410059347a5ef7d6bede964601e
15095801973 <45423456+15095801973@users.noreply.github.com>
2021-07-12 17:48:51 +0800

    添加了消费记录和小标题

--- src/main/java/com/sevengroup/campus/controller/mod3RestController.java
+++ src/main/java/com/sevengroup/campus/controller/mod3RestController.java
@@ -2,6 +2,7 @@ package com.sevengroup.campus.controller;
 
 import com.sevengroup.campus.bean.BookBean;
 import com.sevengroup.campus.bean.UserBean;
+import com.sevengroup.campus.bean.layuiTableBean;
 import com.sevengroup.campus.service.BookService;
 import com.sevengroup.campus.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +22,17 @@ public class mod3RestController {
 
     @Autowired
     BookService bookService;
-
     @GetMapping("/bookInfo")
+
+    public List<BookBean> ALLBookInfo() {
+        List<BookBean> list = new ArrayList<BookBean>();
+        list = bookService.getBookInfo(currentUser);
+        for (BookBean bookBean:list) {
+            System.out.println(bookBean);
+        }
+        return list;
+    }
+    @GetMapping("/selfBookList")
     /**
      * 处理获取借阅图书信息请求
      */
@@ -63,5 +73,42 @@ public class mod3RestController {
         return list;
     }
 
+    @RequestMapping("/charge")
+    public Boolean testBoolean() {
+        return true;
+    }
 
+    @RequestMapping("/inOutRecord")
+    public List<BookBean> inOutInfo() {
+        List<BookBean> list = new ArrayList<BookBean>();
+        list = bookService.getBookInfo(currentUser);
+        for (BookBean bookBean:list) {
+            System.out.println(bookBean);
+        }
+        return list;
+    }
+    @RequestMapping("/payRecord")
+    public List<BookBean> payInfo() {
+        List<BookBean> list = new ArrayList<BookBean>();
+        list = bookService.getBookInfo(currentUser);
+        for (BookBean bookBean:list) {
+            System.out.println(bookBean);
+        }
+        return list;
+    }
+    @RequestMapping("/tableTest")
+    public layuiTableBean tableTest() {
+        layuiTableBean res = new layuiTableBean();
+        res.setCode("0");
+        res.setMsg("");
+        List<BookBean> list = new ArrayList<BookBean>();
+        list = bookService.getBookInfo(currentUser);
+        res.setCount(list.size());
+//        String data="[";
+//        for (BookBean a:list) {
+//            data += a.toString();
+//        }
+        res.setData(list.toString());
+        return res;
+    }
 }
