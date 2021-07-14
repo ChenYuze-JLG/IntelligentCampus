1f8091f8d03156a9d79dc1a1b38eccc9e0173fa7
cquhx <674553522@qq.com>
2021-07-14 15:24:20 +0800

    添加消息显示模块

--- src/main/java/com/sevengroup/campus/controller/MsgController.java
+++ src/main/java/com/sevengroup/campus/controller/MsgController.java
@@ -1,13 +1,25 @@
 package com.sevengroup.campus.controller;
 
+import com.sevengroup.campus.service.MsgService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class MsgController {
 
+    @Autowired
+    MsgService msgService;
+    @Autowired
+    HttpServletRequest request;
+
     @RequestMapping("/messageInfo")
-    String showMessageInfo() {
+    String showMessageInfo(Map<String, Object> map) {
+        String username = (String) request.getSession().getAttribute("username");
+        map.put("msgs", msgService.listMsgs(username));
         return "messageInfo";
     }
 
