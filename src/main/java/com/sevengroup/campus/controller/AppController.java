6dafcf0c9731db9bdd508ab430b7de1d926fa230
15095801973 <45423456+15095801973@users.noreply.github.com>
2021-07-18 11:52:21 +0800

    index.html增加了搜索

--- src/main/java/com/sevengroup/campus/controller/AppController.java
+++ src/main/java/com/sevengroup/campus/controller/AppController.java
@@ -5,10 +5,11 @@ import com.sevengroup.campus.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AppController {
 
     @Autowired
