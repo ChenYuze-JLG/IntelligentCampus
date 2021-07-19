5ad43b23f959cbf7f765d95663a358b32b67aa26
cquhx <674553522@qq.com>
2021-07-19 16:12:46 +0800

    修改无法直接进入首页的bug&数据库密码加密

--- src/main/java/com/sevengroup/campus/controller/tool/Tool.java
+++ src/main/java/com/sevengroup/campus/controller/tool/Tool.java
@@ -3,6 +3,10 @@ package com.sevengroup.campus.controller.tool;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @Title: tool
@@ -17,4 +21,17 @@ public class Tool {
     public String getUserID(HttpServletRequest request) {
         return (String) request.getSession().getAttribute("username");
     }
+
+    public static String getMD5Str(String str) {
+        byte[] digest = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            digest  = md5.digest(str.getBytes("utf-8"));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        //16是表示转换为16进制数
+        return new BigInteger(1, digest).toString(16);
+    }
+
 }
